848ac638226b14b1bee06b8fc8ea924c4958dd08
ljda <3374452761@qq.com>
2025-04-14 22:35:51 +0800

    Site updated: 2025-04-14 22:35:51

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/14/hello-world/","2025/04/14/d”失败/"];function toRandomPost(){
+var posts=["2025/04/14/hello-world/","2025/04/14/上传失败“spwan error”/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
